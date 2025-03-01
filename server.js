b57c4642b0839c59a18d3f3d18189c47f15decd7
ezedin ebrahim <ezedinebrahim131@gmail.com>
2025-03-01 22:12:34 +0300

    commiting the after updating url

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173", // Your frontend URL
+    origin: "https://ethioshop-820b.onrender.com", // Your frontend URL
     methods: ["GET", "POST"],
   },
 });
